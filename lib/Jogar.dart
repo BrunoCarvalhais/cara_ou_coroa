9d0ca6f83f6d75a2a937217866d5c6a497f51326
BrunoCarvalhais <carvalhaisazevedo@gmail.com>
2021-10-21 11:09:05 -0300

    commit3

--- lib/Jogar.dart
+++ lib/Jogar.dart
@@ -1,15 +1,26 @@
+import 'dart:math';
+
+import 'package:cara_ou_coroa/Resultado.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Jogar extends StatefulWidget {
-  const Jogar({Key? key}) : super(key: key);
-
   @override
   _JogarState createState() => _JogarState();
 }
 
 class _JogarState extends State<Jogar> {
-  void _exibirResultado() {}
+  void _exibirResultado() {
+    var itens = ["cara", "coroa"];
+    var numero = Random().nextInt(itens.length);
+    var resultado = itens[numero];
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (contex) => Resultado(resultado),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
